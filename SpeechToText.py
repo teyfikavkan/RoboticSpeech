50b0afd4b94aef28f23b263d8b13732829bccbb4
Benokan <benokankafkas@gmail.com>
2017-11-26 16:37:21 +0300

    default audio update
    a bug about audio has been fixed

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,15 +1,20 @@
 import speech_recognition as sr
+import pyaudio
 
 # talkSpeechToText() function gets our voice by using computer microphone and
 # it returns what we are saying to computer as a string
 def talkSpeechToText():
     r=sr.Recognizer()
 
+    pa = pyaudio.PyAudio()
+    x = pa.get_default_input_device_info()
+    xx = x['index']  # Json objesi dönüyor...
 
-    with sr.Microphone() as source:
+    with sr.Microphone(xx) as source:
 
 
         print("Say Something")
+        r.adjust_for_ambient_noise(source,xx)
         audio=r.listen(source)
         '''r.energy_threshold = 500 '''
         '''Increasing the energy_threshold seemed to resolve this issue for me.
